d09e534441b71bc1513dc22dd38df64c74d0a1eb
sgmotry <mcryouta3@gmail.com>
2026-01-21 07:13:23 +0000

    ポモドーロタイマーのインポート

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import PomodoroTimer from "@/components/PomodoroTimer";
+
 export default function Home() {
   return (
-    <div className="h-[90vh] w-[90vw] font-serif ml-auto mr-auto">
-      <div className="m-5 text-center text-5xl">Simple Pomodoro</div>
+    <div className="h-[90vh] w-[90vw] ml-auto mr-auto">
+      <div className="m-5 text-center text-5xl text-shadow-slate-600 text-shadow-xs">Simple Pomodoro</div>
+      <PomodoroTimer/>
     </div>
   );
 }
