599875d226d953ef7f28819aae0469b42973f9cf
sgmotry <mcryouta3@gmail.com>
2026-01-21 05:34:54 +0000

    画面サイズ調整

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen font-serif">
+    <div className="h-[90vh] w-[90vw] font-serif ml-auto mr-auto">
       <div className="m-5 text-center text-5xl">Simple Pomodoro</div>
     </div>
   );
